6190478ce274fddac7cc4d1e12149b0bb60aa6c1
Martin lei <17988093+MartinLei@users.noreply.github.com>
2023-10-11 22:06:07 +0200

    add test

---
+++ test/controllers/FileListControllerTest.scala
@@ -0,0 +1,33 @@
+package controllers
+
+import models.{FileItem, FileItems}
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.play.*
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.libs.json.Json
+import play.api.test.*
+import play.api.test.Helpers.*
+import service.FileListService
+class FileListControllerTest extends PlaySpec with GuiceOneAppPerTest with Injecting with MockitoSugar:
+  val fileListServiceMock: FileListService = mock[FileListService]
+  val controller = new FileListController(fileListServiceMock, stubControllerComponents())
+
+
+  "/files GET" should {
+
+    "get all given files" in {
+      // setup
+      val expectedFiles = FileItems(List(FileItem("Test", "test2")))
+      when(fileListServiceMock.getAll()).thenReturn(expectedFiles)
+
+      // execute
+      val result = controller.getAll().apply(FakeRequest(GET, "/files"))
+  
+      // verify
+      status(result) mustBe OK
+      contentType(result) mustBe Some("application/json")
+      contentAsJson(result) mustBe Json.toJson(expectedFiles)
+    }
+    
+  }
