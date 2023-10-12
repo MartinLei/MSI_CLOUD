c4716cfdac39bfc75607ea7e695b428927f556f6
Martin lei <17988093+MartinLei@users.noreply.github.com>
2023-10-12 10:31:03 +0200

    insert dummy daten

--- dev-tools/postgresql/init-scripts.sql
+++ dev-tools/postgresql/init-scripts.sql
@@ -4,3 +4,6 @@ CREATE TABLE IF NOT EXISTS fileitem (
     data varchar(255),
     PRIMARY KEY (id)
 );
+
+--INSERT DUMMY DATEN
+INSERT INTO fileitem VALUES ('1','Test1 Name','data1'), ('2','Test2 Name','data2');
