219e04032123e5db2118f4781b2c23f9ecfc385c
JojoGraff <johannes.graff2001@gmail.com>
2023-10-16 09:52:40 +0200

    first frontend impl

--- app/controllers/FileUploadController.scala
+++ app/controllers/FileUploadController.scala
@@ -4,17 +4,18 @@ import play.api.libs.Files
 import play.api.mvc.*
 import service.FileListService
 
+import java.nio.file.Paths
 import javax.inject.*
 
 class FileUploadController @Inject()(cc: ControllerComponents, fileListService: FileListService)
   extends AbstractController(cc):
 
-  def upload(itemName: String): Action[MultipartFormData[Files.TemporaryFile]] = Action(parse.multipartFormData) {
+  def upload(): Action[MultipartFormData[Files.TemporaryFile]] = Action(parse.multipartFormData) {
     request =>
       request.body
         .file("file")
         .map { file =>
-          fileListService.addFileItem(itemName, file)
+          fileListService.addFileItem(Paths.get(file.filename).getFileName.toString, file)
           Ok("File uploaded")
         }
         .getOrElse {
---
+++ public/javascripts/main.js
@@ -0,0 +1,9 @@
+
+const triggerButton = document.getElementById('triggerButton');
+const hiddenInput = document.getElementById('hiddenInput');
+
+triggerButton.addEventListener('click', function() {
+    // Trigger a click event on the hidden input
+    hiddenInput.click();
+});
+
