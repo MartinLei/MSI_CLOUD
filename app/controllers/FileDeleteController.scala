38a6361606f56f47d7c31564885573aeef615bdc
Manuel Günter <guenter.manuel@web.de>
2023-10-16 15:56:24 +0200

    run scalafmt

--- app/controllers/FileDeleteController.scala
+++ app/controllers/FileDeleteController.scala
@@ -3,24 +3,22 @@ package controllers
 import play.api.libs.Files
 import play.api.mvc.*
 import service.FileListService
-import scala.util.{Success, Failure}
+import scala.util.{Failure, Success}
 
 import javax.inject.*
 import scala.concurrent.ExecutionContext.Implicits.global
 
-
-class FileDeleteController @Inject()(cc: ControllerComponents, fileListService: FileListService)
-  extends AbstractController(cc):
+class FileDeleteController @Inject() (cc: ControllerComponents, fileListService: FileListService)
+    extends AbstractController(cc):
 
   def delete(id: Int) = Action.async { request =>
-    fileListService.removeFileItem(id).map { rowsAffected =>
-      if (rowsAffected > 0) {
-        Ok("Element erfolgreich gelöscht")
-      } else {
-        NotFound("Element nicht gefunden")
+    fileListService
+      .removeFileItem(id)
+      .map { rowsAffected =>
+        if rowsAffected > 0 then Ok("Element erfolgreich gelöscht")
+        else NotFound("Element nicht gefunden")
       }
-    }.recover {
-      case ex: Throwable =>
+      .recover { case ex: Throwable =>
         InternalServerError(s"Fehler bei der Datenbankoperation: ${ex.getMessage}")
-    }
+      }
   }
