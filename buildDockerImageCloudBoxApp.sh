eb4c27f2122bd3644ff6700387726bda1f059c9b
Martin Lei <martinlei@users.noreply.github.com>
2023-11-18 16:30:14 +0100

    dockerise app

--- buildDockerImageCloudBoxApp.sh
+++ buildDockerImageCloudBoxApp.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+# build docker images cloudBoxApp
 sbt ";project cloudBoxApp;dist"
 docker build -t cloudboxapp ./cloudBoxApp/.
 
 # TODO Change APPLICATION_SECRET run "sbt project cloudBoxApp playGenerateSecret"
 docker run -e APPLICATION_SECRET='49EOB:1M5<DOXLWa?1B>loWYjEb^tbWxVgm3H1[U`lh1=ER6^zt@ZmeD<aXJX7gR' -e GOOGLE_PROJECT_ID='spring-monolith-403010' -e GOOGLE_BUCKET_NAME='cloudbox-media-bucket' -e GOOGLE_FIRESTORE_COLLECTION_ID='TEST' --mount type=bind,source="$(pwd)"/dev-tools/credentials,target=/srv/credentials/,readonly   -p 8080:8080 cloudboxapp
 
+
+# build docker images imageRecognitionApp
+docker build -t imagerecognitionapp ./imageRecognitionApp/.
+docker run -p 8080:8080 imagerecognitionapp
