c8739f549125004c7c9377ed9c62c3d2de0c6726
Martin Lei <martinlei@users.noreply.github.com>
2023-12-29 15:56:25 +0100

    grap frames

--- animalProtectApp/app/repositories/RTMPCapture.scala
+++ animalProtectApp/app/repositories/RTMPCapture.scala
@@ -1,104 +1,70 @@
 package repositories
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Flow, Sink, Source}
-import akka.util.ByteString
-import org.bytedeco.opencv.opencv_videoio.VideoCapture
-import org.bytedeco.opencv.opencv_core.Mat
-import org.bytedeco.opencv.global.opencv_imgcodecs.*
-
-import scala.concurrent.{ExecutionContextExecutor, Future}
-import java.nio.file.{Files, Paths}
-import org.bytedeco.opencv.global.opencv_core.*
 import org.bytedeco.javacv.FFmpegFrameGrabber
 
 import java.io.File
+import java.util.Calendar
 import javax.imageio.ImageIO
-//import org.bytedeco.javacpp.avcodec
-//import org.bytedeco.javacpp.opencv_core.Buffer
-//import org.bytedeco.javacpp.opencv_core.IplImage
-import org.bytedeco.javacv.CanvasFrame
-import org.bytedeco.javacv.FFmpegFrameGrabber
-import org.bytedeco.javacv.FrameGrabber
+import scala.language.postfixOps
 import org.bytedeco.javacv.*
 
-object RTMPCapture {
+object RTMPCapture:
 
+  def main(args: Array[String]): Unit =
+    val rtmpStreamURL = "rtmp://localhost/live"
+    val myThread = new MyThread(rtmpStreamURL)
 
+    myThread.run()
+    myThread.wait()
 
-  def main(args: Array[String]): Unit = {
-//    val rtmpStreamURL = "rtmp://localhost/live"
-//     val myThread = new MyThread(rtmpStreamURL)
-//
-//    myThread.run()
-//
-//    myThread.wait()
-    old()
-  }
+class MyThread(rtmpStreamURL: String) extends Runnable:
 
-    def old() = {
-      // Replace this with your RTMP stream URL
-      val rtmpStreamURL = "rtmp://localhost/live"
-      
-      val grabber = new FFmpegFrameGrabber(rtmpStreamURL)
-      println("Init")
-      var frame = new Frame()
+  def run(): Unit =
 
-      println("Start")
-      grabber.start()
-      println("Grab")
-      frame = grabber.grabImage
-      println("Stop")
-      grabber.stop()
-      println("release")
-      grabber.release()
-
-      val currentDir = new File(System.getProperty("user.dir"))
-      println(s"Save Path ${currentDir.getAbsolutePath}")
-      val outputFile = new File(currentDir, "output.png") // Save as output.png in the current directory
-
-      println("convert")
-      val converter = new Java2DFrameConverter()
-      val image = converter.getBufferedImage(frame)
-      println("save image")
-      ImageIO.write(image, "png", outputFile)
-    }
-}
-
-class MyThread(rtmpStreamURL: String) extends Runnable {
-
-  def run(): Unit = {
-    val grabber = new FFmpegFrameGrabber(rtmpStreamURL)
-    println("Init")
+    println(s"Init ${Calendar.getInstance().getTime}")
 
     val currentDir = new File(System.getProperty("user.dir") + "/output")
     println(s"Save Path ${currentDir.getAbsolutePath}")
     val converter = new Java2DFrameConverter()
 
-    try {
-      println("Start")
-      grabber.start()
+    var retry = 0
+    while retry <= 5 do
+      try
+        val grabber = FFmpegFrameGrabber.createDefault(rtmpStreamURL)
+        FFmpegFrameGrabber.tryLoad()
+        println("Start")
+        grabber.start()
 
-      for (i <- 1 to 5) {
-        println("Grab")
-        val frame = grabber.grabKeyFrame()
+        retry = 0
+        grabFrame(grabber, currentDir, converter)
 
-        println("convert")
-        val image = converter.getBufferedImage(frame)
-        println("save image")
-        val outputFile = new File(currentDir, s"output_${i}.png")
+        println("Stop")
+        grabber.stop()
+      catch
+        case e: Exception =>
+          val waitSeconds: Int = Math.pow(2, retry).toInt * 10
+          println(s"Connection refused. wait $waitSeconds sec.")
+          Thread.sleep(1000 * waitSeconds)
+          retry = retry + 1
+          if retry == 5 then println("Could not reconnect stop.")
+          else println("Retry to connect.. ")
+
+  private def grabFrame(grabber: FFmpegFrameGrabber, currentDir: File, converter: Java2DFrameConverter): Unit =
+    for i <- 1 to 5000 do
+      println("skip frames ca. 10sec")
+      for i <- 1 to 300 do
+        grabber.grabImage()
+        Thread.sleep(10)
+      println("grab frame")
+      val frame = grabber.grabImage()
+
+      val image = converter.getBufferedImage(frame)
+      if image != null then
+        val filename = s"output_${i}_${Calendar.getInstance().getTime}.png"
+        println(s"save image $filename")
+        val outputFile = new File(currentDir, filename)
         ImageIO.write(image, "png", outputFile)
-
-        Thread.sleep(10000)
+      else {
+        println(s"no image received ${Calendar.getInstance().getTime} -> reconnect")
+        grabber.restart()
       }
-
-    }finally {
-      println("Stop")
-      grabber.stop()
-      println("release")
-      grabber.release()
-    }
-  }
-
-}
